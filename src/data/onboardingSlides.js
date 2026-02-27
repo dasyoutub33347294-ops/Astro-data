0ee498984d5a2376a0394fbf9fc29f3017951bd1
dasyoutub33347294-ops <dasyoutub33347294@gmail.com>
2026-02-27 07:12:05 +0530

    Create onboardingSlides.js

---
+++ src/data/onboardingSlides.js
@@ -0,0 +1,23 @@
+export const ONBOARDING_SLIDES = [
+  {
+    id: 1,
+    image: '/images/onboarding_palm.png', // Placeholder path
+    titleKey: 'onboarding.title1', // Keys match i18n
+    descKey: 'onboarding.desc1',
+    animation: 'fade-in'
+  },
+  {
+    id: 2,
+    image: '/images/onboarding_tarot.png',
+    titleKey: 'onboarding.title2',
+    descKey: 'onboarding.desc2',
+    animation: 'slide-up'
+  },
+  {
+    id: 3,
+    image: '/images/onboarding_ai.png',
+    titleKey: 'onboarding.title3',
+    descKey: 'onboarding.desc3',
+    animation: 'pulse'
+  }
+];
