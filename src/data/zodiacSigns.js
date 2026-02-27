1b0c6ad32a4c4b25e9e2ea9ffaed3847d1d310f8
dasyoutub33347294-ops <dasyoutub33347294@gmail.com>
2026-02-27 07:07:35 +0530

    Create zodiacSigns.js

---
+++ src/data/zodiacSigns.js
@@ -0,0 +1,86 @@
+export const ZODIAC_SIGNS = [
+  {
+    id: 'aries',
+    name: 'Aries',
+    dates: 'Mar 21 - Apr 19',
+    element: 'Fire',
+    symbol: '♈'
+  },
+  {
+    id: 'taurus',
+    name: 'Taurus',
+    dates: 'Apr 20 - May 20',
+    element: 'Earth',
+    symbol: '♉'
+  },
+  {
+    id: 'gemini',
+    name: 'Gemini',
+    dates: 'May 21 - Jun 20',
+    element: 'Air',
+    symbol: '♊'
+  },
+  {
+    id: 'cancer',
+    name: 'Cancer',
+    dates: 'Jun 21 - Jul 22',
+    element: 'Water',
+    symbol: '♋'
+  },
+  {
+    id: 'leo',
+    name: 'Leo',
+    dates: 'Jul 23 - Aug 22',
+    element: 'Fire',
+    symbol: '♌'
+  },
+  {
+    id: 'virgo',
+    name: 'Virgo',
+    dates: 'Aug 23 - Sep 22',
+    element: 'Earth',
+    symbol: '♍'
+  },
+  {
+    id: 'libra',
+    name: 'Libra',
+    dates: 'Sep 23 - Oct 22',
+    element: 'Air',
+    symbol: '♎'
+  },
+  {
+    id: 'scorpio',
+    name: 'Scorpio',
+    dates: 'Oct 23 - Nov 21',
+    element: 'Water',
+    symbol: '♏'
+  },
+  {
+    id: 'sagittarius',
+    name: 'Sagittarius',
+    dates: 'Nov 22 - Dec 21',
+    element: 'Fire',
+    symbol: '♐'
+  },
+  {
+    id: 'capricorn',
+    name: 'Capricorn',
+    dates: 'Dec 22 - Jan 19',
+    element: 'Earth',
+    symbol: '♑'
+  },
+  {
+    id: 'aquarius',
+    name: 'Aquarius',
+    dates: 'Jan 20 - Feb 18',
+    element: 'Air',
+    symbol: '♒'
+  },
+  {
+    id: 'pisces',
+    name: 'Pisces',
+    dates: 'Feb 19 - Mar 20',
+    element: 'Water',
+    symbol: '♓'
+  }
+];
